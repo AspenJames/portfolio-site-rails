a4e724ce6328d8688aefad8d352cc2b1839ac3e3
Aspen James <aspenjames@tqca.org>
2020-02-14 09:41:52 -0800

    Update projects/detail.js to retry loading libs
    Occasionally, the CodeMirror lib/mode that is being dynamically loaded
    in projects/detail.js will fail. Without digging too far into internals
    of RequireJS and tracing down the exact error, this change introspects
    on the state of the actual page after attempting the load. If the libs
    were loaded properly, then the CodeMirror snippet should be present on
    the page. `checkLoaded()` will try to call the `load()` function three
    times in total, with a yet-to-be-determined action for a third failure.

--- app/assets/javascripts/projects/detail.js
+++ app/assets/javascripts/projects/detail.js
@@ -1,11 +1,28 @@
 function main() {
+  let retryCount = 0;
+  const codemirrorDiv = document.querySelector('#codemirror');
+
+  function load() {
 	require(['../assets/codemirror/lib/codemirror', `../assets/codemirror/mode/${codemirrorLang}/${codemirrorLang}`], function(CodeMirror, _) {
-		const codemirrorDiv = document.querySelector('#codemirror');
-		CodeMirror(codemirrorDiv, {
-			value: codemirrorSnippet.replace(/&quot;/g, '\"'),
-			mode: codemirrorLang
-		})
+	  CodeMirror(codemirrorDiv, {
+		value: codemirrorSnippet.replace(/&quot;/g, '\"'),
+		mode: codemirrorLang
+	  });
 	});
+  }
+
+  function checkLoaded() {
+	if (codemirrorDiv.innerHTML.length === 0) {
+	  if (retryCount > 3) {
+		//TODO: take action when can't load
+	  } else {
+		retryCount++;
+		load();
+	  }
+	}
+  }
+
+  checkLoaded();
 }
 
 window.addEventListener('load', main);
