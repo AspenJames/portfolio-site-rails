5feae018c1b2b23a7e017683a4f4776180e55927
Aspen James <aspenjames@tqca.org>
2020-02-18 15:04:41 -0800

    revise tknbn example

--- app/assets/code/tknbn.rb
+++ app/assets/code/tknbn.rb
@@ -7,17 +7,3 @@ end
 def items_most_recently_updated_at
   self.items.map(&:updated_at).max || self.updated_at
 end
-
-## Below as appears in the source, with sorbet type annotations
-
-sig {returns(T::Array[Project])}
-def self.most_recently_updated
-  T.unsafe(self).all.sort do |a, b|
-    b.items_most_recently_updated_at <=> a.items_most_recently_updated_at
-  end
-end
-
-sig {returns(Time)}
-def items_most_recently_updated_at
-  T.unsafe(self).items.map(&:updated_at).max || T.unsafe(self).updated_at
-end
