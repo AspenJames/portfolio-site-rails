4188bd231152dcde73a8ca709f5ee836baa8ec74
Aspen James <aspenjames@tqca.org>
2020-03-03 13:43:56 -0800

    Whitespace fix, add path_helper for project_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-	root :to => 'static#about'
-	get '/resume', :to => 'static#resume'
+  root :to => 'static#about'
+  get '/resume', :to => 'static#resume'
 
-	get '/projects', :to => 'projects#index'
-	get '/projects/:slug', :to => 'projects#detail'
+  get '/projects', :to => 'projects#index'
+  get '/projects/:slug', :to => 'projects#detail', as: 'project'
 end
