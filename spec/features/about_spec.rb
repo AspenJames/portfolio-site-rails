56d5513b050097558d43c00b8e96e29f32ea397b
Aspen James <aspenjames@tqca.org>
2020-03-04 11:57:51 -0800

    clean up

--- spec/features/about_spec.rb
+++ spec/features/about_spec.rb
@@ -2,33 +2,32 @@ require 'rails_helper'
 require File.expand_path('../support/nav_matcher', __dir__)
 
 RSpec.describe "About", type: :feature do
-  it "contains the proper heading" do
+  before(:each) do
     visit '/'
+  end
+
+  it "contains the proper heading" do
     heading = find('h1')
     expect(heading).to have_text(/Aspen James/)
   end
 
   it "contains navigation links" do
-    visit '/'
     expect(page).to have_navlinks(
       ['/', '/projects', 'https://dev.to/aspenjames', '/resume']
     )
   end
 
   it "has class 'active' on navlink for '/'" do
-    visit '/'
     link = find_link(href: '/')
     expect(link).to be_active()
   end
 
   it "contains proper email link" do
-    visit '/'
     link = find_link(text: "Email me")
     expect(link[:href]).to eq("mailto:aspenjames@tqca.org")
   end
 
   it "contains proper phone link" do
-    visit '/'
     link = find_link(text: "Phone me")
     expect(link[:href]).to eq("tel:+12062902148")
   end
--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -1,33 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe "/projects", type: :feature do
-  it "has class 'active' on navlink for '/projects'" do
+  before(:each) do
     visit '/projects'
+  end
+
+  it "has class 'active' on navlink for '/projects'" do
     link = find_link(href: '/projects')
     expect(link).to be_active()
   end
 
   it "contains a heading and ul" do
-    visit '/projects'
     expect(page).to have_selector("h1")
     expect(page).to have_selector("ul")
   end
 
   it "has a number of li elements equal to the length of projects_json" do
-    visit '/projects'
     lis = page.find_all('li')
     expect(lis.length).to eq(projects_json.length)
   end
 
   context "List items" do
-    before(:all) { @projects = projects }
-
     it "each contain a link to its associated Project#detail page" do
-      visit '/projects'
+      projs = projects # projects is calculated each time it's called
       lis = page.find_all('li')
 
       lis.each_with_index do |li, i|
-        project = @projects[i]
+        project = projs[i]
 
         expect(li).to have_selector('a')
         a = li.find('a')
@@ -40,14 +39,12 @@ RSpec.describe "/projects", type: :feature do
   context "Fun facts" do
     context "on initial render" do
       it "contains a div#funfact-div that is empty" do
-        visit '/projects'
         expect(page).to have_selector('div#funfact-div')
         div = page.find('div#funfact-div')
         expect(div.text).to eq("")
       end
 
       it "contains a button#funfact-button with text 'Yes please!'" do
-        visit '/projects'
         expect(page).to have_selector('button#funfact-button')
         button = page.find_button(id: 'funfact-button')
         expect(button).to have_text("Yes please!")
@@ -55,12 +52,7 @@ RSpec.describe "/projects", type: :feature do
     end
 
     context "on clicking the funfact-button", js: true do
-      after(:each) do
-        page.cleanup!
-      end
-
       it "populates the funfact-div" do
-        visit '/projects'
         div = page.find('div#funfact-div')
         click_button(id: 'funfact-button')
         expect(div.text).not_to be("")
@@ -68,20 +60,17 @@ RSpec.describe "/projects", type: :feature do
       end
 
       it "changes the funfact-div on each button press" do
-        visit '/projects'
         5.times do
           expect{ click_button(id: 'funfact-button') }.to change(find_by_id('funfact-div'), :text)
         end
       end
 
       it "changes the button text exactly once" do
-        visit '/projects'
         button = find_button(id: 'funfact-button')
         expect{ click_button(id: 'funfact-button') }.to change(button, :text)
           .from("Yes please!").to("Another!")
         expect{ click_button(id: 'funfact-button') }.not_to change(button, :text)
       end
     end
-
   end
 end
