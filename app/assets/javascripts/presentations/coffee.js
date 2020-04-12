6782012388d5c8a67609162986c66a369fe0d884
Aspen James <aspenjames@tqca.org>
2020-04-11 21:27:36 -0700

    finish out presentation

--- app/assets/javascripts/presentations/coffee.js
+++ app/assets/javascripts/presentations/coffee.js
@@ -7,6 +7,7 @@ bgButtons.forEach((button) => {
 function removeBackground(event) {
   const button = event.target;
   const photoUrl = button.parentElement.dataset.backgroundImage;
+  const blackTextElements = [...document.querySelectorAll(".black")];
   const divArr = [...document.querySelectorAll("div")];
   const currentDiv = divArr.filter(
     (div) =>
@@ -15,14 +16,16 @@ function removeBackground(event) {
   )[0];
 
   currentDiv.style.backgroundImage = "none";
+  blackTextElements.forEach((el) => el.classList.remove("black"));
   button.removeEventListener("click", removeBackground);
   button.addEventListener("click", addBackground);
   button.innerText = "Bring the photo back";
 
   function addBackground() {
     currentDiv.style.backgroundImage = `url("${photoUrl}")`;
-    button.innerText = "I can't read this";
+    blackTextElements.forEach((el) => el.classList.add("black"));
     button.removeEventListener("click", addBackground);
     button.addEventListener("click", removeBackground);
+    button.innerText = "I can't read this";
   }
 }
